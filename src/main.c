b4719a2f3ad8e25de0e121e3ec0c6724b100fae6
root <root@localhost>
2024-10-01 11:57:20 +0300

    auto publish 1727773039583

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int Age,Amount;
+printf("enter age: ");
+scanf("%d", &Age);
+if(age>=21)
+printf("congratulations you qualify for a loan\n");
+else
+printf("unfortunately,we are unble to offer you loan at this time.\n");
+printf("enter amount");
+scanf("%d, &amount");
+if(amount>=21000)
+printf("congratulation you qualify for a loan");
+else
+printf("unfortunately,we are unable to offer you loan at this time.");
+return 0;
+}
